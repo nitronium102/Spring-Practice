0b950f5c0f1aee3cf261d99d1d91357b074758c2
Minji Kang <nitronium@ewhain.net>
2022-02-03 00:15:31 +0900

    다국어 처리를 위한 Internationalization 구현
    1) yml 파일에 spring.messages.basename={사용할 properties 파일의 이름}
    2) resources 밑에 {yml 파일에서 설정한 이름}.properties를 만들어준다 -> 다국어 버전으로 여러 개 생성
    3) @SpringBootApplication 밑에 @Bean을 생성해서 localeResolver 생성
    4) Controller에서 설정 -> Header로 language 값을 받아서 해당 언어에 맞는 메세지를 출력

--- RESTful-web-service/src/main/java/com/example/restfulwebservice/RestfulWebServiceApplication.java
+++ RESTful-web-service/src/main/java/com/example/restfulwebservice/RestfulWebServiceApplication.java
@@ -2,12 +2,26 @@ package com.example.restfulwebservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class RestfulWebServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulWebServiceApplication.class, args);
+	}
+
+	@Bean
+	// @SpringBootApplication에 @Bean을 등록하게 되면 Spring Boot가 초기화될 떄,
+	// 해당 정보에 해당되는 값이 메모리에 올라가서 다른 클래스가 사용할 수 있게 된다.
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.KOREA); // default : 한국어 처리
+		return localeResolver;
 	}
 
 }
--- RESTful-web-service/src/main/java/com/example/restfulwebservice/helloworld/HelloWorldController.java
+++ RESTful-web-service/src/main/java/com/example/restfulwebservice/helloworld/HelloWorldController.java
@@ -1,13 +1,21 @@
 package com.example.restfulwebservice.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController // @Controller + @RequestBody : view를 갖지 않는 REST Data(JSON/XML) 반환 -> 자동으로 ResponseBody에 넣어준다
 public class HelloWorldController {
-	// GET
-	// /hello-world (endpoint)
+
+	@Autowired // annotation을 통한 의존성 주입 : spring framework에 등록되어 있는 bean 중 같은 타입을 가진 bean을 자동으로 주입
+	private MessageSource messageSource;
+
+	// GET /hello-world (endpoint)
 	@GetMapping("/hello-world")
 	public String helloWorld() {
 		return "Hello world";
@@ -21,6 +29,18 @@ public class HelloWorldController {
 	@GetMapping("/hello-world-bean/path-variable/{name}")
 	public HelloWorldBean helloWorldBean(@PathVariable String name) { // @PathVariable : 이 변수가 가변 데이터임을 명시
 		return new HelloWorldBean(String.format("Hello World, %s", name)); // json 형식으로 반환
+	}
+
+	@GetMapping(path="/hello-world-internationalized")
+	public String helloWorldInternationalized(
+		@RequestHeader(name="Accept-Language", required = false) Locale locale) {
+		// "Accept-Language" 값이 설정되지 않았을 때는 자동으로 default값인 한국어로 설정(기본 locale 값)
+		// "Accept-Language" 가 설정되어 있으면 locale에 저장
+
+		// 첫번째 인자: 설정 파일에서의 key 값
+		// 두번째 인자: 만약 parameter를 가진 가변변수라면 parameter 지정
+		// 세번째 인자: RequestHeader를 통해 전달받은 locale
+		return messageSource.getMessage("greeting.message", null, locale);
 	}
 
 }
