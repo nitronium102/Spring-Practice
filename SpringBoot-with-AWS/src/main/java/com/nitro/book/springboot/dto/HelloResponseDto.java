29f48588ac74433449d940177e88b29db8b7665f
Minji Kang <nitronium@ewhain.net>
2022-02-24 23:44:33 +0900

    2. 테스트 코드 작성

---
+++ SpringBoot-with-AWS/src/main/java/com/nitro/book/springboot/dto/HelloResponseDto.java
@@ -0,0 +1,15 @@
+package com.nitro.book.springboot.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+	private final String name;
+	private final int amount;
+
+}
+
