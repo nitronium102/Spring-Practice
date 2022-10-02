1ac62b1081a20c27ba7d73911c5729b090e60a00
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2022-10-02 17:19:28 +0900

    [JUnit5] 테스트 반복하기

--- Java-with-Test/src/main/java/com/example/javawithtest/Study.java
+++ Java-with-Test/src/main/java/com/example/javawithtest/Study.java
@@ -6,12 +6,14 @@ public class Study {
 	private StudyStatus status = StudyStatus.DRAFT;
 
 	private int limit;
+	private String name;
 
-	public Study(int limit) {
+	public Study(int limit, String name) {
 		if (limit < 0) {
 			throw new IllegalArgumentException("limit은 0보다 커야 한다");
 		}
 		this.limit = limit;
+		this.name = name;
 	}
 
 	public StudyStatus getStatus() {
@@ -20,5 +22,18 @@ public class Study {
 
 	public int getLimit() {
 		return limit;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public String toString() {
+		return "Study{" +
+				"status=" + status +
+				", limit=" + limit +
+				", name='" + name + '\'' +
+				'}';
 	}
 }
--- Java-with-Test/src/test/java/com/example/javawithtest/StudyTest.java
+++ Java-with-Test/src/test/java/com/example/javawithtest/StudyTest.java
@@ -5,6 +5,18 @@ import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.OS;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.aggregator.AggregateWith;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregationException;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregator;
+import org.junit.jupiter.params.converter.ArgumentConversionException;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.converter.SimpleArgumentConverter;
+import org.junit.jupiter.params.provider.*;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.Convert;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.time.Duration;
 
@@ -27,6 +39,54 @@ class StudyTest {
 	@DisplayName("스터디 만들기 slow")
 	void create1() { // java reflection을 사용하면 private, default인 메소드에서 접근 가능
 		System.out.println("create2");
+	}
+
+	@DisplayName("스터디 만들기")
+	@RepeatedTest(value = 10, name = "{displayName}, {currentRepetition}/{totalRepetitions}")
+	void repeatTest(RepetitionInfo repetitionInfo) {
+		System.out.println("test" + repetitionInfo.getCurrentRepetition() + "/" + repetitionInfo.getTotalRepetitions());
+	}
+
+	@DisplayName("스터디 만들기")
+	@ParameterizedTest(name = "{index} {displayName} message={0}") // parameter를 인덱스로 설정 가능
+	@ValueSource(strings = {"날씨가", "많이", "추워지고", "있네요."}) // 1개의 parameter
+	void parameterizedTest(String message) {
+		System.out.println(message);
+	}
+
+	/* argumentAggregator */
+	@DisplayName("스터디 만들기3")
+	@ParameterizedTest(name = "{index} {displayName} message={0}")
+	@CsvSource({"10, '자바 스터디'", "20, 스프링"})
+	@NullAndEmptySource
+	void aggregationTest(@AggregateWith(StudyAggregator.class) Study study) {
+		System.out.println(study);
+	}
+
+	static class StudyAggregator implements ArgumentsAggregator { // static inner / public class여야 함
+
+		@Override
+		public Object aggregateArguments(ArgumentsAccessor argumentsAccessor, ParameterContext parameterContext) throws ArgumentsAggregationException {
+			return new Study(argumentsAccessor.getInteger(0), argumentsAccessor.getString(1));
+		}
+	}
+
+	/* argumentConvereter */
+	@DisplayName("스터디 만들기2")
+	@ParameterizedTest(name = "{index} {displayName} message={0}")
+	@ValueSource(ints = {10, 20, 40})
+	@NullAndEmptySource
+	void convertionTest(@ConvertWith(StudyConverter.class) Study study) {
+		System.out.println(study.getLimit());
+	}
+
+	static class StudyConverter extends SimpleArgumentConverter {
+
+		@Override
+		protected Object convert(Object source, Class<?> targetType) throws ArgumentConversionException {
+			assertEquals(Study.class, targetType, "Can only convert to Study");
+			return new Study(Integer.parseInt(source.toString()), "이름");
+		}
 	}
 
 	@BeforeAll // 모든 테스트가 실행이 되기 전에 딱 한 번만 실행
