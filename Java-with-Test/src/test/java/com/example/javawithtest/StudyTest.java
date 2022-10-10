f3362449fb0d5443bf175e41a7c41d9e625f962d
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2022-10-10 14:56:03 +0900

    [JUnit5] 테스트 인스턴스
    JUnit은 테스트 메소드를 독립적으로 실행하여 예상치 못한 부작용을 방지하기 위해 테스트 메소드 마다 테스트 인스턴스를 새로 만든다.
    이 전략을 JUnit5에서는 @TestInstance(Lifecycle.PER_CLASS)를 사용해서 변경할 수 있다. 이 경우, @BeforeAll과 @AfterAll에서 static을 사용하지 않아도 괜찮다

--- Java-with-Test/src/test/java/com/example/javawithtest/StudyTest.java
+++ Java-with-Test/src/test/java/com/example/javawithtest/StudyTest.java
@@ -1,32 +1,12 @@
 package com.example.javawithtest;
 
-import jdk.jfr.Enabled;
 import org.junit.jupiter.api.*;
-import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
-import org.junit.jupiter.api.condition.EnabledOnOs;
-import org.junit.jupiter.api.condition.OS;
-import org.junit.jupiter.api.extension.ParameterContext;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.aggregator.AggregateWith;
-import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
-import org.junit.jupiter.params.aggregator.ArgumentsAggregationException;
-import org.junit.jupiter.params.aggregator.ArgumentsAggregator;
-import org.junit.jupiter.params.converter.ArgumentConversionException;
-import org.junit.jupiter.params.converter.ConvertWith;
-import org.junit.jupiter.params.converter.SimpleArgumentConverter;
-import org.junit.jupiter.params.provider.*;
-import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.Convert;
-import org.springframework.beans.factory.annotation.Value;
-
-import java.time.Duration;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assumptions.assumeTrue;
-import static org.junit.jupiter.api.Assumptions.assumingThat;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class StudyTest {
+
+	int value = 1;
 
 	@FastTest // 오타를 줄이는 용도로 사용가능
 	@DisplayName("스터디 만들기 fast")
@@ -41,61 +21,13 @@ class StudyTest {
 		System.out.println("create2");
 	}
 
-	@DisplayName("스터디 만들기")
-	@RepeatedTest(value = 10, name = "{displayName}, {currentRepetition}/{totalRepetitions}")
-	void repeatTest(RepetitionInfo repetitionInfo) {
-		System.out.println("test" + repetitionInfo.getCurrentRepetition() + "/" + repetitionInfo.getTotalRepetitions());
-	}
-
-	@DisplayName("스터디 만들기")
-	@ParameterizedTest(name = "{index} {displayName} message={0}") // parameter를 인덱스로 설정 가능
-	@ValueSource(strings = {"날씨가", "많이", "추워지고", "있네요."}) // 1개의 parameter
-	void parameterizedTest(String message) {
-		System.out.println(message);
-	}
-
-	/* argumentAggregator */
-	@DisplayName("스터디 만들기3")
-	@ParameterizedTest(name = "{index} {displayName} message={0}")
-	@CsvSource({"10, '자바 스터디'", "20, 스프링"})
-	@NullAndEmptySource
-	void aggregationTest(@AggregateWith(StudyAggregator.class) Study study) {
-		System.out.println(study);
-	}
-
-	static class StudyAggregator implements ArgumentsAggregator { // static inner / public class여야 함
-
-		@Override
-		public Object aggregateArguments(ArgumentsAccessor argumentsAccessor, ParameterContext parameterContext) throws ArgumentsAggregationException {
-			return new Study(argumentsAccessor.getInteger(0), argumentsAccessor.getString(1));
-		}
-	}
-
-	/* argumentConvereter */
-	@DisplayName("스터디 만들기2")
-	@ParameterizedTest(name = "{index} {displayName} message={0}")
-	@ValueSource(ints = {10, 20, 40})
-	@NullAndEmptySource
-	void convertionTest(@ConvertWith(StudyConverter.class) Study study) {
-		System.out.println(study.getLimit());
-	}
-
-	static class StudyConverter extends SimpleArgumentConverter {
-
-		@Override
-		protected Object convert(Object source, Class<?> targetType) throws ArgumentConversionException {
-			assertEquals(Study.class, targetType, "Can only convert to Study");
-			return new Study(Integer.parseInt(source.toString()), "이름");
-		}
-	}
-
 	@BeforeAll // 모든 테스트가 실행이 되기 전에 딱 한 번만 실행
-	static void beforeAll() {
+	void beforeAll() {
 		System.out.println("before all");
 	}
 
 	@AfterAll
-	static void afterAll() {
+	void afterAll() {
 		System.out.println("after all");
 	}
 
