5e10e4195fa5af32570d59e8b6ee636bbb45bcd0
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2022-10-10 15:23:36 +0900

    [JUnit5] 확장 모델
    - 선언적인 등록 @ExtendWith
    - 프로그래밍 등록 @RegisterExtension

--- Java-with-Test/src/test/java/com/example/javawithtest/StudyTest.java
+++ Java-with-Test/src/test/java/com/example/javawithtest/StudyTest.java
@@ -1,13 +1,17 @@
 package com.example.javawithtest;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.RegisterExtension;
 
-@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+//@ExtendWith(FindSlowTestExtension.class) // 선언적인 등록
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class StudyTest {
 
 	int value = 1;
+
+	@RegisterExtension
+	static FindSlowTestExtension findSlowTestExtension = new FindSlowTestExtension(1000L);
 
 	@Order(2)
 	@FastTest // 오타를 줄이는 용도로 사용가능
@@ -20,7 +24,8 @@ class StudyTest {
 	@Order(1)
 	@SlowTest
 	@DisplayName("스터디 만들기 slow")
-	void create1() { // java reflection을 사용하면 private, default인 메소드에서 접근 가능
+	void create1() throws InterruptedException { // java reflection을 사용하면 private, default인 메소드에서 접근 가능
+		Thread.sleep(1005L);
 		System.out.println("create2");
 	}
 
