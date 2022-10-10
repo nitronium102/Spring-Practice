825459a410358c0620e8e88e56b14471cb7d2624
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2022-10-10 15:00:06 +0900

    [JUnit5] 테스트 순서
    테스트 메소드를 원하는 순서에 따라 실행하도록 @TestInstance(Lifecycle.PER_CLASS)와 함께 @TestMethodOrder를 사용할 수 있다.

--- Java-with-Test/src/test/java/com/example/javawithtest/StudyTest.java
+++ Java-with-Test/src/test/java/com/example/javawithtest/StudyTest.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.*;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class StudyTest {
 
 	int value = 1;
 
+	@Order(2)
 	@FastTest // 오타를 줄이는 용도로 사용가능
 	@DisplayName("스터디 만들기 fast")
 	void create_new_study() { // java reflection을 사용하면 private, default인 메소드에서 접근 가능
@@ -15,6 +17,7 @@ class StudyTest {
 		String test_env = System.getenv("TEST_ENV");
 	}
 
+	@Order(1)
 	@SlowTest
 	@DisplayName("스터디 만들기 slow")
 	void create1() { // java reflection을 사용하면 private, default인 메소드에서 접근 가능
@@ -22,12 +25,12 @@ class StudyTest {
 	}
 
 	@BeforeAll // 모든 테스트가 실행이 되기 전에 딱 한 번만 실행
-	void beforeAll() {
+	static void beforeAll() {
 		System.out.println("before all");
 	}
 
 	@AfterAll
-	void afterAll() {
+	static void afterAll() {
 		System.out.println("after all");
 	}
 
