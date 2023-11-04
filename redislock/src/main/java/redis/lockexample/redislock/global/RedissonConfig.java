adabd1def0aecc3d94b8f5c9078166294d2691b6
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2023-11-04 23:31:21 +0900

    feat: Redisson Config 추가

---
+++ redislock/src/main/java/redis/lockexample/redislock/global/RedissonConfig.java
@@ -0,0 +1,30 @@
+package redis.lockexample.redislock.global;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedissonConfig {
+
+	@Value("${spring.data.redis.host}")
+	private String redisHost;
+
+	@Value("${spring.data.redis.port}")
+	private int redisPort;
+
+	// redission 연결을 위해 호스트 주소 앞에 붙임
+	private static final String REDISSION_HOST_PREFIX = "redis://";
+
+	@Bean
+	public RedissonClient redissonClient() { // redissonClient를 생성하고 반환
+		RedissonClient redissonClient = null;
+		Config config = new Config();
+		config.useSingleServer().setAddress(REDISSION_HOST_PREFIX + redisHost + ":" + redisPort);
+		redissonClient = Redisson.create(config);
+		return redissonClient;
+	}
+}
